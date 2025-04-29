0a9e09e554fd8e560bb31be7cfa330fb12d73230
starc-industries <ajith17796@gmail.com>
2025-04-29 16:55:06 +0530

    Add files via upload

---
+++ sendmail/index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>send email</title>
+</head>
+<body>
+<form class="" action="send.php" method="post">
+Email<input type="email" name="email" value""><br>
+Subject<input type="text" name="subject" value""><br>
+Message<input type="text" name="message" value""><br>
+<button type="submit" name="send">send</button>
+</body>
+</html>
